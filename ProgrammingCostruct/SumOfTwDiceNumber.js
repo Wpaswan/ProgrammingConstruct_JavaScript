d8f976268b08bac80fe42f08b8e859e48c865036
Wpaswan <dablumcapaswan@gmail.com>
2022-01-08 10:03:09 -0800

    [I added a code to add two dice numbers]

---
+++ ProgrammingCostruct/SumOfTwDiceNumber.js
@@ -0,0 +1,6 @@
+
+
+    var diceRoll1 = Math.floor( Math.random() * 6 )+1;
+    var diceRoll2=Math.floor(Math.random()*6)+1;
+    var sumOfTWoDiceNumber=diceRoll1+diceRoll2
+    console.log(sumOfTWoDiceNumber)
