18f530d15c7d19671ae15ddc4bd0d111ab4d35df
Wpaswan <dablumcapaswan@gmail.com>
2022-01-09 07:43:07 -0800

    [I added a code to convert digit into word]

---
+++ ProgrammingCostruct/DigitIntoWord.js
@@ -0,0 +1,37 @@
+"use strict"
+const ps=require('prompt-sync')
+const prompt=ps();
+var SingleDigitNumber=parseInt(prompt("Enter a single digit number:"))
+switch(SingleDigitNumber)
+{
+    case 0:
+        console.log("Zero!!")
+        break;
+        case 1:
+            console.log("One")
+            break;
+            case 2:
+                 console.log("TWo!!")
+            break;
+        case 3:
+            console.log("Three!!")
+            break;
+            case 4:
+        console.log("Four!!")
+            break;
+        case 5:
+            console.log("Five!!")
+            break;
+            case 6:
+                console.log("Six!!");
+                break;
+                case 7:
+                    console.log("Seven!!");
+                    break;
+                    case 8:
+                        console.log("Eight!!");
+                        break;
+                        case 9:
+                            console.log("Nine!!");
+
+}
